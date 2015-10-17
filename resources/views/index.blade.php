fd5ead5a574fdcfdca330d8c6b895e2e9ae3b087
YLitv <lemanruss12yehor.lytvynov@yandex.ru>
2015-10-17 22:03:57 +0300

    no cache

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -132,7 +132,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="/js/jquery.cookie.js"></script>
-<script src="/js/application.js"></script>
+<script src="/js/application.js?{!! time() !!}"></script>
 
 </body>
 </html>
